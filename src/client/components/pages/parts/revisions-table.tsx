1151e18bfa558d5066c6916288f32c3e49cbdbdf
meziyum <siddhant24feb@gmail.com>
2023-07-25 15:04:42 +0530

    Fix Copyright

--- src/client/components/pages/parts/revisions-table.tsx
+++ src/client/components/pages/parts/revisions-table.tsx
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Prabal Singh
+ *  			 2023 Meziyum
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
